55672c6c551abb7da29adc2510d0c0f51deccc92
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-09-18 19:07:58 -0500

    Add files via upload

---
+++ print_statements_song.py
@@ -0,0 +1,32 @@
+person = input("Enter the name of a person: ")
+animal = input("Enter the plural of any animal: ")
+color = input("Enter a color: ")
+example = input("Enter a noun: ")
+place = input("Enter a place: ")
+action = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+adjective = input("Enter an adjective: ")
+
+
+
+
+
+print(f"Twinkle, twinkle, little {person},")
+print(f"How I wonder what {animal} are!")
+print(f"Up above the {example} so high,")
+print(f"Like a diamond in the {place}.")
+print(f"How I wonder what {animal} are! \n")
+
+print(f"When the blazing sun is {adjective},")
+print(f"When the nothing shines {adverb},")
+print("Then you show your little light,")
+print("Twinkle, twinkle, all the night.")
+print(f"How I wonder what you {action}! \n")
+
+print(f"Then the {example} in the dark,")
+print(f"Thanks you for your {color} spark,")
+print("He could not see which way to go,")
+print(f"If {person} did not twinkle so")
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are! \n")
+
