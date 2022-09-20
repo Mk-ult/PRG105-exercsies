09fd2be350f3d13227072f40b2738600efc2f191
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-09-19 19:59:01 -0500

    Add files via upload

--- budget_program.py
+++ budget_program.py
@@ -2,7 +2,7 @@ total_monthly_income = float(input("How much is your total monthly income? "))
 monthly_housing = float(input("How much do you spend on housing each month? "))
 monthly_food = float(input("How much do you spend on food each month? "))
 monthly_transportation = float(input("How much do you spend on transportation each month? "))
-monthly_phonebill = float(input("How much do you spend on your phone bill each month? "))
+monthly_phone_bill = float(input("How much do you spend on your phone bill each month? "))
 monthly_utilities = float(input("How much do you spend on utilities each month? "))
 monthly_clothing = float(input("How much do you spend on clothing each month? "))
 
@@ -14,9 +14,15 @@ total_food = monthly_food / total_monthly_income
 print(f"Food takes up {total_food:.2%} of your monthly income")
 total_transportation = monthly_transportation / total_monthly_income
 print(f"Transportation takes up {total_transportation:.2%} of your monthly income")
-total_phone = monthly_phonebill / total_monthly_income
+total_phone = monthly_phone_bill / total_monthly_income
 print(f"Your phone bill takes up {total_phone:.2%} of your monthly income")
 total_utilities = monthly_utilities / total_monthly_income
 print(f"Utilities take up {total_utilities:.2%} of your monthly income")
 total_clothing = monthly_clothing / total_monthly_income
 print(f"Clothing takes up {total_clothing:.2%} of your monthly income")
+
+print("\n")
+
+total_spent = monthly_housing + monthly_food + monthly_transportation + monthly_phone_bill + monthly_utilities + monthly_clothing
+leftover_spending = total_monthly_income - total_spent
+print(f"You have ${leftover_spending:.2f} left from your income after paying these monthly expenses")
