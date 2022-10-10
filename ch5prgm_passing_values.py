1bf96b7d266f2f6a5e6ad231724ce64e0e603faa
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-10-09 21:21:06 -0500

    Add files via upload

---
+++ ch5prgm_passing_values.py
@@ -0,0 +1,7 @@
+# Create a program that asks a user to enter a whole number between 20 and 100
+# Send that number to a function that will validate the number
+# and if it is not a number between 20 and 100, use a while loop to keep asking the user for a valid number
+# Return the number to the main function
+# In addition, create three functions that determine if the number is divisible by two, by three, and by five
+# Pass all of the results to a final function that displays output on the screen - identifying whether or not the number is divisible by two, three, and five.
+
