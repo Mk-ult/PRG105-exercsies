4114a19610fff3337842024fdf45bdc75e337bf7
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-10-09 21:29:15 -0500

    Add files via upload

--- ch5prgm_passing_values.py
+++ ch5prgm_passing_values.py
@@ -5,3 +5,47 @@
 # In addition, create three functions that determine if the number is divisible by two, by three, and by five
 # Pass all of the results to a final function that displays output on the screen - identifying whether or not the number is divisible by two, three, and five.
 
+
+def main():
+    user_number = int(input("Enter a whole number between 20 and 100: "))
+    good_number = validate(user_number)
+    two = divisible_by_two(good_number)
+    three = divisible_by_three(good_number)
+    five = divisible_by_five(good_number)
+    output(good_number, two, three, five)
+    
+    
+def validate(number):
+    while number < 20 or number > 100:
+        print("That is not a valid number")
+        number = int(input("Enter a whole number between 20 and 100: "))
+    return number
+
+
+def divisible_by_two(number):
+    if number % 2 == 0:
+        return " is divisible by two"
+    else:
+        return " is not divisible by two"
+    
+    
+def divisible_by_three(number):
+    if number % 3 == 0:
+        return " is divisible by three"
+    else:
+        return " is not divisible by three"
+    
+    
+def divisible_by_five(number):
+    if number % 5 == 0:
+        return " is divisible by five"
+    else:
+        return " is not divisible by five"
+    
+    
+def output(number, two_result, three_result, five_result):
+    print(str(number) + two_result)
+    print(str(number) + three_result)
+    print(str(number) + five_result)
+
+main()
