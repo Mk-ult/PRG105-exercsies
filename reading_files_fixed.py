85bca6b2c240b68c226e9d9e7c917a7e1d64d4a8
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-10-16 19:52:07 -0500

    Add files via upload

---
+++ reading_files_fixed.py
@@ -0,0 +1,14 @@
+def main():
+    sales_file = open("sales_totals.txt", "r")
+    total = 0
+    count = 0
+    for line in sales_file:
+        sales_amt = float(line.rstrip('\n'))
+        print(format(sales_amt, "10,.2f"))
+        total += sales_amt
+        count += 1
+    print("Total: " + format(total, "27,.2f"))
+    print("Number of entries: " + format(count, "15,.0f"))
+    print("Average: " + format((total/count), "25,.2f"))
+
+main()
