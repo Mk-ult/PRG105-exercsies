06b3bf962a9b1a39cfa4bb1334dfe85db9276ec3
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-10-30 15:17:54 -0500

    Add files via upload

--- rainfall.py
+++ rainfall.py
@@ -12,23 +12,23 @@ def main():
 
     total = 0.0
     count = 0
-    amount = []
 
     for line in lines:
-        line = line.rstrip()
-        line = line.split()
-        month = line[0]
-        count += 1
-        # print(month)
-        # print(amount)
+        line = line.rstrip("\n")
+        rain = line.split()
+        rain_month = rain[0]
+        rain_amt = rain[1].split('.')
 
-        try:
-            amount = float(line[1])
-            total += amount
-        except ValueError:
-            print("The value for", month, "is", amount)
-    print(f"Total rainfall: {total:.2f}")
-    print(f"Average rainfall: {total / count:.2f}")
+        if rain_amt[0].isdigit() and rain_amt[1].isdigit():
+            amount = float(rain_amt[0] + "." + rain_amt[1])
+            total += float(amount)
+            count += 1
+        else:
+            print(rain_month + " does not have valid data because")
+            print(str(rain[1] + " is not a number"))
+
+    print("the total rainfall for " + str(count) + " months is " + format(total, ',.2f'))
+    print("the average rainfall was " + format((total/count), ',.2f'))
 
 
 main()
