dfef9a093563661e3ee9a5ed1b7ffd065d62d639
Martin K <113481156+Mk-ult@users.noreply.github.com>
2022-11-25 18:18:37 -0600

    Add files via upload

--- GIT Chapter Exercises/ch12_exercises.py
+++ GIT Chapter Exercises/ch12_exercises.py
@@ -12,15 +12,18 @@ print("=" * 10, "Section 12.1 simple recursion", "=" * 10)
 # 1) Using program 12-2 as an example, create a recursive function.
 #    The function should print "Hooray!" the number of times requested
 #    by the parameter times_requested
-def main():
-    message(5)
+
+
+def hooray(times_requested):
+    if times_requested > 1:
+        hooray(times_requested -1)
+    print("Hooray!")
     
-def message(times_requested):
-    if times_requested > 0:
-        print("Hooray!")
-        message(times_requested - 1)
+    
 # 2) Call the function with a parameter value of 5.
-main()
+hooray(10)
+
+
 # TODO 12.2-12.3 problem solving with recursion
 print("=" * 10, "Section 12.2-12.3 problem solving with recursion", "=" * 10)
 # 1) Create a function that uses recursion to sum all of the numbers in a list.
@@ -28,16 +31,20 @@ print("=" * 10, "Section 12.2-12.3 problem solving with recursion", "=" * 10)
 #    and it should return the sum of the list values.
 # Hint: Each iteration should remove one item from the list.
 # The recursion should end when all items have been removed from the list.
-def main2():
-    def sum_list(numbers):
-        if len(numbers) == 1:
-            return numbers[0]
-        else:
-            return numbers[0] + sum_list(numbers[1:])
+
+
+def sum_list(list_of_numbers):
+    if len(list_of_numbers) > 1:
+        return list_of_numbers[0] + sum_list(list_of_numbers[1:])
+    else:
+        return list_of_numbers[0]
+    
+
 # 2) Call the function using the numbers list as a parameter
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    print(sum_list(numbers))
-main2()
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(sum_list(numbers))
 
 
 
